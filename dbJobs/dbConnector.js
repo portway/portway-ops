0c194c398a97b8b79685bcc5de64bf34f3d429c4
Dirk Heniges <dheniges@gmail.com>
2020-07-08 15:43:33 -0700

    self signed ssl options

--- dbJobs/dbConnector.js
+++ dbJobs/dbConnector.js
@@ -1,12 +1,17 @@
 const { Pool } = require("pg");
 
+const sslConfig = {
+  // allow self-signed certs on digital ocean
+  rejectUnauthorized: false
+}
+
 const config = {
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: process.env.DB_USE_SSL === 'true'
+  ssl: process.env.DB_USE_SSL === 'true' ? sslConfig : false
 }
 
 const pool = new Pool(config);
