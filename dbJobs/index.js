31969aadd5c17728c67ee09f736125fb0673a5eb
Dirk Heniges <dheniges@gmail.com>
2020-12-11 17:44:43 -0800

    oops

--- dbJobs/index.js
+++ dbJobs/index.js
@@ -4,7 +4,7 @@ const db = require('./dbConnector')
 // key is the accepted argument for this script
 const scripts = {
   documentSlugs: 'createDocumentSlugs',
-  deleteOrphanedRecords: 'setDeletedRecords'
+  setDeletedRecords: 'setDeletedRecords'
 }
 const validScriptNames = Object.keys(scripts)
 
